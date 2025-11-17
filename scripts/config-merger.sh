aa649d592e8fff6f40331a4de08552928a8f8374
Fazle Rabbi Sarker <fazlerabbi@appscode.com>
2025-11-17 21:14:44 +0600

    remove extra volume
    Signed-off-by: Fazle Rabbi Sarker <fazlerabbi@appscode.com>

--- scripts/config-merger.sh
+++ scripts/config-merger.sh
@@ -3,10 +3,7 @@
 set -eo pipefail
 set -x
 
-
 ELASTICSEARCH_UID=${ELASTICSEARCH_UID:-1000}
-# directory for default config files
-DEFAULT_CONFIG_DIR=/elasticsearch/config-share/default-config
 # directory for operator generated files
 TEMP_CONFIG_DIR=/elasticsearch/temp-config
 # directory for user provided custom files
@@ -26,9 +23,6 @@ if [[ "$(id -u)" == "0" ]]; then
   echo "changing the ownership of data folder: /usr/share/elasticsearch/data"
   chown -R "$ELASTICSEARCH_UID":"$ELASTICSEARCH_UID" /usr/share/elasticsearch/data
 fi
-
-# load default config files to config directory
-cp -f -R $DEFAULT_CONFIG_DIR/* $CONFIG_DIR
 
 # For Elasticsearch config directory
 for FILE_DIR in "$CONFIG_DIR"/*; do
@@ -107,14 +101,12 @@ for FILE_DIR in "$CONFIG_DIR"/*; do
 done
 
 ##----------------------------------------Elasticsearch Keystore------------------------------------
-
 # For secure settings
 # On "$ /usr/share/elasticsearch/bin/elasticsearch-keystore create" command,
 # elasticsearch.keystore file is created at config directory.
 # Create the keystore, later add the secure settings to keystore.
 # Since the keystore is generated even before starting the main container, no need to
 # restart/reload the secure settings.
-
 if [ -d $SECURE_SETTINGS_DIR ]; then
     echo "Updating secure settings..."
     if [ -f $CONFIG_DIR/elasticsearch.keystore ]; then
@@ -144,6 +136,3 @@ if [ -d $SECURE_SETTINGS_DIR ]; then
         done
     fi
 fi
-
-
-

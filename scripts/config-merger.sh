139162a7e969a64713eb9002761055d93607e0d9
Fazle Rabbi Sarker <73359077+fr-sarker@users.noreply.github.com>
2026-01-15 19:19:03 +0600

    configure-reconfigure process for OSversion-2.19 (#106)
    Signed-off-by: Fazle Rabbi Sarker <fazlerabbi@appscode.com>

--- scripts/config-merger.sh
+++ scripts/config-merger.sh
@@ -16,6 +16,8 @@ CONFIG_DIR=/usr/share/opensearch/config
 DEFAULT_SECURITY_CONFIG_DIR=/elasticsearch/default-securityconfig
 # directory for security config files
 SECURITY_CONFIG_DIR=/usr/share/opensearch/config/opensearch-security
+#inline-Config
+INLINE_CONFIG="inline"
 
 # List of comma seperated roles
 # NODE_ROLES="master, ingest, data" or NODE_ROLES="master"
@@ -57,6 +59,12 @@ for FILE_DIR in "$CONFIG_DIR"/*; do
             yq merge -i --overwrite "$FILE_DIR" $CUSTOM_CONFIG_DIR/"$FILE_NAME"
         fi
 
+        #merge inline-config file with the updated one
+        INLINE_CONFIG_FILE_NAME="$INLINE_CONFIG-$FILE_NAME"
+        if [ -f $TEMP_CONFIG_DIR/"$INLINE_CONFIG_FILE_NAME" ]; then
+            yq merge -i --overwrite "$FILE_DIR" $TEMP_CONFIG_DIR/"$INLINE_CONFIG_FILE_NAME"
+        fi
+
         for RoleName in "${ROLES[@]}"; do
             # remove leading and trailing spaces
             RoleName=$(echo $RoleName)
@@ -73,6 +81,11 @@ for FILE_DIR in "$CONFIG_DIR"/*; do
             # merge user provided custom config with the updated one
             if [ -f $CUSTOM_CONFIG_DIR/"$ROLE_FILE_NAME" ]; then
                 yq merge -i --overwrite "$FILE_DIR" $CUSTOM_CONFIG_DIR/"$ROLE_FILE_NAME"
+            fi
+            #merge inline-config files with the updated one
+            INLINE_CONFIG_ROLE_FILE_NAME="$INLINE_CONFIG-$ROLE_FILE_NAME"
+            if [ -f $TEMP_CONFIG_DIR/"$INLINE_CONFIG_ROLE_FILE_NAME" ]; then
+                yq merge -i --overwrite "$FILE_DIR" $TEMP_CONFIG_DIR/"$INLINE_CONFIG_ROLE_FILE_NAME"
             fi
         done
     else
@@ -141,6 +154,11 @@ if [ -d $SECURITY_CONFIG_DIR ]; then
             if [ -f $CUSTOM_CONFIG_DIR/"$FILE_NAME" ]; then
                 yq merge -i --overwrite "$FILE_DIR" $CUSTOM_CONFIG_DIR/"$FILE_NAME"
             fi
+            #merge inline-config file with the updated one
+            INLINE_CONFIG_FILE_NAME="$INLINE_CONFIG-$FILE_NAME"
+            if [ -f $TEMP_CONFIG_DIR/"$INLINE_CONFIG_FILE_NAME" ]; then
+                yq merge -i --overwrite "$FILE_DIR" $TEMP_CONFIG_DIR/"$INLINE_CONFIG_FILE_NAME"
+            fi
 
             for RoleName in "${ROLES[@]}"; do
                 # remove leading and trailing spaces
@@ -158,6 +176,12 @@ if [ -d $SECURITY_CONFIG_DIR ]; then
                 # merge user provided custom config with the updated one
                 if [ -f $CUSTOM_CONFIG_DIR/"$ROLE_FILE_NAME" ]; then
                     yq merge -i --overwrite "$FILE_DIR" $CUSTOM_CONFIG_DIR/"$ROLE_FILE_NAME"
+                fi
+
+                #merge inline-config files with the updated one
+                INLINE_CONFIG_ROLE_FILE_NAME="$INLINE_CONFIG-$ROLE_FILE_NAME"
+                if [ -f $TEMP_CONFIG_DIR/"$INLINE_CONFIG_ROLE_FILE_NAME" ]; then
+                    yq merge -i --overwrite "$FILE_DIR" $TEMP_CONFIG_DIR/"$INLINE_CONFIG_ROLE_FILE_NAME"
                 fi
             done
         else
